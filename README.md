0930532dc9ba6ab7037f62a659490dd6ad54224f
Santo <santokhan1999@gmail.com>
2023-09-20 14:33:51 +0600

    initial

---
+++ README.md
@@ -0,0 +1,7 @@
+# Docs
+
+<https://flowbite.com/docs/getting-started/vue/>
+
+<https://flowbite-vue.com/pages/getting-started>
+
+Repo: <https://github.com/santokhan/amadershop.git>
---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initFlowbite();
+})
+
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
