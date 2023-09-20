b302f0a89f635f1e6a9f88183992f9a2c3f4abbb
Santo <santokhan1999@gmail.com>
2023-09-21 00:05:50 +0600

    update

--- src/components/Testimonial.vue
+++ src/components/Testimonial.vue
@@ -1,5 +1,8 @@
 <template>
-    <PageReviews />
+    <div class="mt-16">
+        <h2 class="text-3xl lg:text-4xl font-semibold text-main text-center">আমাদের গ্রাহকরা কি বলে</h2>
+        <PageReviews />
+    </div>
 </template>
 
 <script setup lang="ts">
